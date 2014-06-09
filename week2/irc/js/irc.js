44473bf56510928da1eacceb9aad253332bab31e
unknown <clintonfreeman@RN-CFREEMAN-01.corp.infusiondev.com>
2014-06-09 11:46:10 -0400

    added new week2 lab

---
+++ week2/irc/js/irc.js
@@ -0,0 +1,9 @@
+(function () {
+	'use strict';
+
+	// Here's a blank JavaScript file to start you off. Note the IIFE
+	// to create scope, and the use strict directive.
+
+	// Think about how to structure your code though; maybe it doesn't
+	// make sense to put it all into one file.
+})
